737adcdc9841dd86d726e0ee510a6d90b1ccf828
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-02-17 12:18:05 -0500

    Chore: styled-components

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,15 +4,19 @@ import styled from 'styled-components';
 function Navbar() {
   return (
     <ContainerNavbar>
-      <ul>
-        <li><a href='#'>Home</a></li>
-        <li><a href='#'>Calendar</a></li>
-        <li><a href='#'>Our Ministries</a></li>
-        <li><a href='#'>Helpful Infomation</a></li>
-        <li><a href='#'>Contacts</a></li>
-        <li><a href='#'>Online Giving</a></li>
-        <li><a href='#'>Our Media</a></li>
-      </ul>
+      <NavbarHeader>
+				<NavbarInfo>
+				<ul>
+					<li><a href='#'>Home</a></li>
+					<li><a href='#'>Calendar</a></li>
+					<li><a href='#'>Our Ministries</a></li>
+					<li><a href='#'>Helpful Infomation</a></li>
+					<li><a href='#'>Contacts</a></li>
+					<li><a href='#'>Online Giving</a></li>
+					<li><a href='#'>Our Media</a></li>
+      	</ul>
+				</NavbarInfo>
+			</NavbarHeader>
     </ContainerNavbar>
   )
 }
@@ -22,7 +26,11 @@ export default Navbar;
 const ContainerNavbar = styled.div`
     height: 65vh;
     width: 260px;
+		flex: 0.3;
+		color: whitesmoke;
 		background-color: lightsalmon;
-	
-
 `;
+
+const NavbarHeader = styled.div``;
+
+const NavbarInfo = styled.div``;
